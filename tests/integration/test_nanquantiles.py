73d18675e08d87afb2902511c572dcc315fa263c
XiaLuNV <110973296+XiaLuNV@users.noreply.github.com>
2025-01-07 10:44:34 +0800

    enhance test_nanquantiles.py (#549)

--- tests/integration/test_nanquantiles.py
+++ tests/integration/test_nanquantiles.py
@@ -39,7 +39,7 @@ ALL_METHODS = (
 
 
 @pytest.mark.parametrize("str_method", ALL_METHODS)
-@pytest.mark.parametrize("axes", (0, 1))
+@pytest.mark.parametrize("axes", (0, 1, (0, 1), (0,)))
 @pytest.mark.parametrize(
     "qin_arr", (0.5, [0.001, 0.37, 0.42, 0.67, 0.83, 0.99, 0.39, 0.49, 0.5])
 )
