8058c796eee9beb48d2cd531ef83ede463aff1a8
manopapad <manopapad@users.noreply.github.com>
2025-05-15 17:41:19 -0400

    Deploying to gh-pages from @ nv-legate/cupynumeric.internal@b93298dd8f7ddc1e3b65624110dc27340aaad0af 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '25.05.00.dev+50.gc612727e',
+    VERSION: '25.05.00.dev+52.gb93298dd',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
