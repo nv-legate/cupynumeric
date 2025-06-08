feb3d96443cf8e247cb31789e16f1d769dd2e8fc
mag1cp1n <mag1cp1n@users.noreply.github.com>
2025-06-08 19:20:05 -0400

    Deploying to gh-pages from @ nv-legate/cupynumeric.internal@acad6a1bdea72b9d5a5afa9f4733ab037677b54a 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '25.07.00.dev+2.gca23321b',
+    VERSION: '25.07.00.dev+3.gacad6a1b',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
