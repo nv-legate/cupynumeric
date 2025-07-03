6eb5de32e198fd39bbba5811c2e4369793589abb
Irina Demeshko <idemeshko@nvidia.com>
2025-07-03 14:14:09 -0700

    adding missing includes (#858)

--- src/cupynumeric/matrix/syev.cu
+++ src/cupynumeric/matrix/syev.cu
@@ -20,6 +20,7 @@
 
 #include <thrust/execution_policy.h>
 #include <thrust/iterator/transform_iterator.h>
+#include <thrust/iterator/counting_iterator.h>
 
 #include "cupynumeric/cuda_help.h"
 #include <vector>
