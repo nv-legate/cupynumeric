44e357a46d69ac6a014de2d117bd1ba4b8b1b3d1
marcinz <marcinz@users.noreply.github.com>
2025-08-05 13:14:18 -0400

    Deploying to gh-pages from @ nv-legate/cupynumeric.internal@d443fe63653827101adb0b428d3157bd80f691ab 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '25.08.00.dev+27.gaa521d10',
+    VERSION: '25.08.00.dev+30.gd443fe63',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
