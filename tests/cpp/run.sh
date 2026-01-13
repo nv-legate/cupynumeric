01fa22000c58b4df76c08e58ff1260227d97e561
Mona Han <129724851+mona-nv@users.noreply.github.com>
2026-01-13 14:22:23 +0800

    Fix the cpp test path issue (#1346)

--- tests/cpp/run.sh
+++ tests/cpp/run.sh
@@ -2,12 +2,12 @@
 
 if [ $# -eq 0 ]
   then
-    REALM_BACKTRACE=1 LEGATE_TEST=1 python run.py
+    LEGATE_AUTO_CONFIG=0 REALM_BACKTRACE=1 LEGATE_TEST=1 python run.py
 elif [ $# -eq 1 ]  && [ "$1" = "ctest" ]
   then
     echo "Using ctest"
     cd build
-    REALM_BACKTRACE=1 LEGATE_TEST=1 LEGION_DEFAULT_ARGS="-ll:cpu 4" ctest --output-on-failure "$@"
+    LEGATE_AUTO_CONFIG=0 REALM_BACKTRACE=1 LEGATE_TEST=1 LEGION_DEFAULT_ARGS="-ll:cpu 4" ctest --output-on-failure "$@"
 else
     echo "Invalid arguments"
 fi
