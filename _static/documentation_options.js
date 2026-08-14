ca53529ea80f3b35d662fa4dd42fcbf432702dd2
XiaLuNV <XiaLuNV@users.noreply.github.com>
2026-08-14 03:46:49 -0400

    Deploying to gh-pages from @ nv-legate/cupynumeric.internal@85a201d90ba67397aa091c714dcf8a969720cd54 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '26.07.00.dev+119.gf0de4869',
+    VERSION: '26.07.00.dev+120.g85a201d9',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
